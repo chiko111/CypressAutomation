e9864b34dfd4f3e4c335ea6eba8f6012496b0c05
chiko111 <chiko.walchev@gmail.com>
2023-02-17 14:14:30 +0200

    readdy

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'aa3ob1',
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
